7c81cb1bca56b26eca724213e48969cb39ff3fb1
Hector Garcia <hectorgarciami@gmail.com>
2013-10-30 21:48:23 -0700

    modified db

--- flaskr.py
+++ flaskr.py
@@ -1,55 +1,86 @@
-# all the imports
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-	  abort, render_template, flash
-from contextlib import closing
-	  
-# configuration
-DATABASE =  '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY ='development key' 
-USERNAME = 'admin'
-PASSWORD = 'default'
+# -*- coding: utf-8 -*-
+"""
+Flaskr
+~~~~~~
 
-#create application:
+A microblog example application written as Flask tutorial with
+Flask and sqlite3.
+
+:copyright: (c) 2010 by Armin Ronacher.
+:license: BSD, see LICENSE for more details.
+"""
+
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+#from contextlib import closing
+
+# create our little application :)
 app = Flask(__name__)
-app.config.from_object(__name__)
 
-#define method to connect to db. Will come handy later
+# Load default config and override config from an environment variable. Works only if environvariable is available - HECTOR
+app.config.update(dict(
+    DATABASE='/tmp/flaskr.db',
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)		#from_object is replaced by from_envvar() to load config from config file
+															#That way someone can set an environment variable called FLASKR_SETTINGS to
+															#specify a config file to be loaded and replace default values
+
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE'])
-	
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
 def init_db():
-	with closing(connect_db()) as db:
-		with app.open_resource('schema.sql', mode='r') as f:
-			db.cursor().executescript(f.read())
-		db.commit()
+    """Creates the database tables."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
-@app.before_request
-def before_request():
-	g.db = connect_db()
 
-@app.teardown_request
-def teardown_request(exception):
-	db = getattr(g, 'db', None)
-	if db is not None:
-		db.close()
+def get_db():
+    """Opens a new database connection if there is none yet for the
+current application context.
+"""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
+
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
                  [request.form['title'], request.form['text']])
-    g.db.commit()
+    db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -65,12 +96,14 @@ def login():
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
-		
+
 
 if __name__ == '__main__':
-	app.run()
+    init_db()				#stops the need to run init_db() manually
+    app.run()
